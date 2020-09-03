d37bc48bea5bf59737e21b75c318eb569e9740d3
Ricardo Manoel <rdomanoel@gmail.com>
2020-09-03 12:48:28 -0300

    player jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,23 +5,59 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float runSpeed = 5f;
-    Rigidbody2D myRigidBody;
-    // Start is called before the first frame update
-    void Start()
-    {
-        myRigidBody = GetComponent<Rigidbody2D>();
-    }
+  [SerializeField] float runSpeed = 5f;
+  [SerializeField] float jumpSpeedy = 5f;
+  // State
+  bool isAlive = true;
+  Rigidbody2D myRigidBody;
+  Animator myAnimator;
+  Collider2D myColider2D;
 
-    // Update is called once per frame
-    void Update()
-    {
-        Run();
-    }
 
-    private void Run() {
-        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
-        myRigidBody.velocity = playerVelocity;
+  // Start is called before the first frame update
+  void Start()
+  {
+    myRigidBody = GetComponent<Rigidbody2D>();
+    myAnimator = GetComponent<Animator>();
+    myColider2D = GetComponent<Collider2D>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    Run();
+    Jump();
+    FlipSprite();
+  }
+
+  private void Run()
+  {
+    float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+    bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+
+    Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
+    myRigidBody.velocity = playerVelocity;
+
+    myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+  }
+
+  private void Jump()
+  {
+    if (!myColider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) return;
+
+    if (CrossPlatformInputManager.GetButtonDown("Jump"))
+    {
+      Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeedy);
+      myRigidBody.velocity += jumpVelocityToAdd;
     }
+  }
+  private void FlipSprite()
+  {
+    bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+
+    if (playerHasHorizontalSpeed)
+    {
+      transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+    }
+  }
 }
